c4d2374c6396fa13f0d1740c22044b1802a84ef3
sociocreatives <ayaraerick@gmail.com>
2025-03-07 23:28:36 +0300

    database change

--- mrkim/settings.py
+++ mrkim/settings.py
@@ -82,12 +82,24 @@ WSGI_APPLICATION = 'mrkim.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mrkim',
+        'USER': 'ovasis',
+        'PASSWORD': 'Omondi12!',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 # DATABASES = {
 #     'default': {
